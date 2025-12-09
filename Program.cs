7e11a4a2bf86564dd16aa053fc8472535aed83d3
Romain Lecesne <niamor2305@gmail.com>
2025-12-09 22:31:48 +0100

    prép

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ namespace Salle_de_Sport
 {
     internal static class Program
     {
-        Console.WriteLine("Hello World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
     }
 }
